edcb6c4fd88a8e3543aead699490e52c2f0cb0d3
Tom Chen <thomas.chen@students.olin.edu>
2015-04-30 11:32:36 -0400

    user-configurable server/client ip address :ice_cream:

--- HangmanClient.py
+++ HangmanClient.py
@@ -4,32 +4,36 @@ from Libraries import utilities
 class HangmanClient(object):
     
     def __init__(self):
-        self.server_addr = ('192.168.66.65',99)
-        self.client_addr = ('192.168.66.65',98)
-        self.sock = CN_Sockets.socket(2,2)
-        self.sock.settimeout(20.0)
-        self.sock.bind(self.client_addr)
-        
-        while True:
-            try:
-                str_message = input("Enter guess:\n").upper()
-                if not str_message or not str_message.isalpha():
-                    continue
-                bytearray_message = bytearray(str_message,encoding="UTF-8")
-                self.sock.sendto(bytearray_message, self.server_addr)
-                
-                print("Waiting for a response...")
-                bytearray_reply, source_address = self.sock.recvfrom(8192)
-                reply = (utilities.forcedecode(bytearray_reply)).replace("7","_")
-                
-                if reply[0:15] == "CONGRATULATIONS" or reply[0:9] == "GAME OVER":
-                    print(reply)
-                    self.sock.close()
-                    sys.exit()
-                else:
-                    print(reply)
-            except CN_Sockets.TimeoutException:
-                print("Server timed out.")
+        svr_ip = input("What's the server's IP?")
+        svr_port = input("What's the server's port?")
+        clt_ip = input("What's my IP?")
+        clt_port = input("What's my port?")
+        self.server_addr = (svr_ip,int(svr_port))
+        self.client_addr = (clt_ip,int(clt_port))
+        with CN_Sockets.socket(2,2) as self.sock:
+            self.sock.settimeout(20.0)
+            self.sock.bind(self.client_addr)
+            
+            while True:
+                try:
+                    str_message = input("Enter guess:\n").upper()
+                    if not str_message or not str_message.isalpha():
+                        continue
+                    bytearray_message = bytearray(str_message,encoding="UTF-8")
+                    self.sock.sendto(bytearray_message, self.server_addr)
+                    
+                    print("Waiting for a response...")
+                    bytearray_reply, source_address = self.sock.recvfrom(8192)
+                    reply = (utilities.forcedecode(bytearray_reply)).replace("7","_")
+                    
+                    if reply[0:15] == "CONGRATULATIONS" or reply[0:9] == "GAME OVER":
+                        print(reply)
+                        self.sock.close()
+                        sys.exit()
+                    else:
+                        print(reply)
+                except CN_Sockets.TimeoutException:
+                    print("Server timed out.")
           
                     
 if __name__ == "__main__":
--- HangmanServer.py
+++ HangmanServer.py
@@ -4,93 +4,94 @@ from Libraries import utilities
 class HangmanServer(object):
    
     def __init__(self):
-        self.server_addr = ('192.168.66.65',99)
-        self.client_addr = ('192.168.66.65',98)
-        self.sock = CN_Sockets.socket(2,2)
-        self.sock.settimeout(2.0)
-        self.sock.bind(self.server_addr)
-        self.original_word = input("Enter a word to use: ").upper()
-        if not(self.original_word.isalpha()):
-            print("Letters only, please.")
-            self.sock.close()
-            sys.exit()
-        self.word = self.original_word
-        self.guessed = []
-        self.lives = 5
-        
-        def print_and_reply(reply):
-            print(reply)
-            self.sock.sendto(reply.encode("UTF-8"),self.client_addr)
-        
-        while True:
-            try:
-                bytearray_msg, source_address = self.sock.recvfrom(8192)
-                source_IP, source_port = source_address
-                message = utilities.forcedecode(bytearray_msg)
-                print(message)
-                
-                if len(message) == 1:
-                    letter = message.upper()
-                    if letter.isalpha():
-                        print("Guess: " + letter)
-                        
-                        if letter in self.guessed:
-                            already_guessed_reply = "Letter has already been guessed"
-                            print_and_reply(already_guessed_reply)
+        svr_ip = input("What's my IP?")
+        svr_port = input("What's my port?")
+        clt_ip = input("What's the client's IP?")
+        clt_port = input("What's the client's port?")
+        self.server_addr = (svr_ip, int(svr_port))
+        self.client_addr = (clt_ip, int(clt_port))
+        with CN_Sockets.socket(2,2) as self.sock:
+            self.sock.settimeout(2.0)
+            self.sock.bind(self.server_addr)
+            self.original_word = input("Enter a word to use: ").upper()
+            if not(self.original_word.isalpha()):
+                print("Letters only, please.")
+                self.sock.close()
+                sys.exit()
+            self.word = self.original_word
+            self.guessed = []
+            self.lives = 5
+            
+            def print_and_reply(reply):
+                print(reply)
+                self.sock.sendto(reply.encode("UTF-8"),self.client_addr)
+            
+            while True:
+                try:
+                    bytearray_msg, source_address = self.sock.recvfrom(8192)
+                    source_IP, source_port = source_address
+                    message = utilities.forcedecode(bytearray_msg)
+                    print(message)
+                    
+                    if len(message) == 1:
+                        letter = message.upper()
+                        if letter.isalpha():
+                            print("Guess: " + letter)
                             
-                        elif letter in self.word:
-                            self.guessed.append(letter)
-                            self.word = self.word.replace(letter,"_")
-                            
-                            complete = True
-                            for char in self.word:
-                                if char != "_":
-                                    complete = False
-                            if complete == True:
-                                victory_reply = "Congratulations The word was " + self.original_word
-                                print_and_reply(victory_reply)
-                                self.sock.close()
-                                sys.exit()
-                            else:
-                                word_length = len(self.original_word)
-                                display_word = "7" * word_length
-                                for i in range(word_length):
-                                    if self.word[i] == "_":
-                                        display_word = display_word[:i] + self.original_word[i] + display_word[i+1:]
+                            if letter in self.guessed:
+                                already_guessed_reply = "Letter has already been guessed"
+                                print_and_reply(already_guessed_reply)
                                 
-                                valid_reply = "Word so far " + display_word
-                                self.sock.sendto(valid_reply.encode("UTF-8"),self.client_addr)
-                                print("Remaining Letters " + self.word)
+                            elif letter in self.word:
+                                self.guessed.append(letter)
+                                self.word = self.word.replace(letter,"_")
+                                
+                                complete = True
+                                for char in self.word:
+                                    if char != "_":
+                                        complete = False
+                                if complete == True:
+                                    victory_reply = "Congratulations The word was " + self.original_word
+                                    print_and_reply(victory_reply)
+                                    self.sock.close()
+                                    sys.exit()
+                                else:
+                                    word_length = len(self.original_word)
+                                    display_word = "7" * word_length
+                                    for i in range(word_length):
+                                        if self.word[i] == "_":
+                                            display_word = display_word[:i] + self.original_word[i] + display_word[i+1:]
+                                    
+                                    valid_reply = "Word so far " + display_word
+                                    self.sock.sendto(valid_reply.encode("UTF-8"),self.client_addr)
+                                    print("Remaining Letters " + self.word)
+                                    
+                            else:
+                                self.guessed.append(letter)
+                                self.lives -= 1
+                                
+                                if self.lives <= 0:
+                                    game_over_reply = "Game over The word was " + self.original_word
+                                    print_and_reply(game_over_reply)
+                                    self.sock.close()
+                                    sys.exit()
+                                    
+                                else:
+                                    incorrect_reply = "Sorry that guess was incorrect You have " + str(self.lives) + " chances remaining"
+                                    print_and_reply(incorrect_reply)
                                 
                         else:
-                            self.guessed.append(letter)
-                            self.lives -= 1
-                            
-                            if self.lives <= 0:
-                                game_over_reply = "Game over The word was " + self.original_word
-                                print_and_reply(game_over_reply)
-                                self.sock.close()
-                                sys.exit()
-                                
-                            else:
-                                incorrect_reply = "Sorry that guess was incorrect You have " + str(self.lives) + " chances remaining"
-                                print_and_reply(incorrect_reply)
+                            invalid_reply = "Invalid input packet received"
+                            print_and_reply(invalid_reply)
                             
                     else:
                         invalid_reply = "Invalid input packet received"
                         print_and_reply(invalid_reply)
-                        
-                else:
-                    invalid_reply = "Invalid input packet received"
-                    print_and_reply(invalid_reply)
-                        
-            except CN_Sockets.TimeoutException:
-                sys.stdout.write(".")
-                sys.stdout.flush()
-                continue
+                            
+                except CN_Sockets.TimeoutException:
+                    sys.stdout.write(".")
+                    sys.stdout.flush()
+                    continue
             
-            
-
-
 if __name__ == "__main__":
     HangmanServer()
